a1b4c4a996209e565e12ef16a5f9bbb7787e83e6
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2025-11-11 15:24:18 +0100

    feat(benchmark): improve plots

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import rc
 
-df = pd.read_csv("benchmark/dfs/benchmark_results.csv")
+rc("font", **{"family": "serif", "serif": ["Computer Modern"]})
+rc("text", usetex=True)
+
+filename = "1D-long"
+
+df = pd.read_csv(f"benchmark/dfs/{filename}.csv")
 df = df.melt(
     id_vars="n_samples",
     value_vars=["python_newton_time", "rust_newton_time", "mosek_time"],
@@ -17,12 +24,39 @@ df["variable"] = df["variable"].replace(
 
 
 # plot python_newton_time vs rust_newton_time as a function of n_samples
-plt = sns.lineplot(data=df, x="n_samples", y="value", hue="variable")
-plt.set(
+plt1 = sns.lineplot(data=df, x="n_samples", y="value", hue="variable")
+plt1.set(
     yscale="log",
     xlabel="Size of the SDP (n_samples)",
     ylabel="Time (s)",
     title="1D Sinusoid",
 )
-plt.legend(title="Solver")
-plt.figure.savefig("benchmark/plots/benchmark_plot.pdf", bbox_inches="tight")
+plt1.legend(title="Solver")
+plt1.figure.savefig(f"benchmark/plots/{filename}.pdf", bbox_inches="tight")
+
+# close plot
+plt.close()
+
+# compute the average time for each n_samples and each method
+df_avg = df.groupby(["n_samples", "variable"]).mean().reset_index()
+# pivot the dataframe to have python and rust times in columns
+df_pivot = df_avg.pivot(
+    index="n_samples", columns="variable", values="value"
+).reset_index()
+# compute the ratio of python over rust
+df_pivot["ratio"] = (
+    df_pivot["Damped Newton (Python)"] / df_pivot["Damped Newton (Rust)"]
+)
+
+# filter to only keep ratio
+df_ratio = df_pivot[["n_samples", "ratio"]]
+print(df_ratio)
+
+plt2 = sns.lineplot(data=df_ratio, x="n_samples", y="ratio")
+plt2.set(
+    # yscale="log",
+    xlabel="Size of the SDP (n_samples)",
+    ylabel="Time Ratio (Python / Rust)",
+    title="1D Sinusoid",
+)
+plt2.figure.savefig(f"benchmark/plots/{filename}_ratio.pdf", bbox_inches="tight")
